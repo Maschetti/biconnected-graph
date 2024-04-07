dc93573b1ae2380d0489971bbe326157d47ca84a
Maschetti <mateusvmaschietto@gmail.com>
2024-04-07 19:58:58 -0300

    finish has cycle and fix DFS

--- main.py
+++ main.py
@@ -14,6 +14,39 @@ def testGraph():
             [7, 9]
         ]
 
+def visit_edge(origin, destiny):
+    return None
+
+def DFS_(graph, node, TD, TT, father, t):
+    t[0] += 1
+    TD[node] = t[0]
+
+    for v in graph[node]:
+        if TD[v] == 0:
+            father[v] = node
+            DFS_(graph, v, TD, TT, father, t)
+        elif (TT[v] == 0) and father[node] != v:
+            visit_edge(node, v)
+    
+    t[0] += 1
+    TT[node] = t[0]
+
+def DFS(graph):
+    t = [0]
+    TD = []
+    TT = []
+    father = []
+    for i in range(len(graph)):
+        TD.append(0)
+        TT.append(0)
+        father.append(None)
+    
+    for i in range(len(graph)):
+        if TD[i] == 0:
+            DFS_(graph, i, TD, TT, father, t)
+    
+    return father, TD, TT
+
 def createRandomGraph(v):
     graph = [[] for _ in range(v)]
     min = int(v)
@@ -35,6 +68,10 @@ def createRandomGraph(v):
     
     # assure the graph is connected
     father, TD, TF = DFS(graph)
+
+    for line in graph:
+        print(line)
+
     roots = [i for i, x in enumerate(father) if x == None]
     for i in range(len(roots) - 1):
         graph[roots[i]].append(roots[i + 1])
@@ -42,46 +79,9 @@ def createRandomGraph(v):
 
     return graph 
 
-def visit_edge(origin, destiny):
-    return None
-
-def DFS_(graph, node, TD, TT, father, t):
-    t = t + 1
-    TD[node] = t
-
-    for v in graph[node]:
-        if TD[v] == 0:
-            father[v] = node
-            DFS_(graph, v, TD, TT, father, t)
-        elif (TT[v] == 0) and not (father[node] == v):
-            visit_edge(node, v)
-    
-    t = t + 1
-    TT[node] = t
-
-def DFS(graph):
-    t = 0
-    TD = []
-    TT = []
-    father = []
-    for i in range(len(graph)):
-        TD.append(0)
-        TT.append(0)
-        father.append(None)
-    
-    for i in range(len(graph)):
-        if TD[i] == 0:
-            DFS_(graph, i, TD, TT, father, t)
-    
-    return father, TD, TT
-
 def findCycle(graph, TD, TT, start, node):
     for n in graph[node]:
-        print(n)
         if TD[n] <= TD[start] and TT[n] >= TT[start]:
-            print(node, "->", n)
-            print(TD[start], "->", TD[n])
-            print(TT[start], "->", TT[n])
             return True
         if TD[node] == TD[n] + 1:
             findCycle(graph, TD, TT, start, n)
@@ -89,26 +89,21 @@ def findCycle(graph, TD, TT, start, node):
     return False
 
 def hasCicle(graph, TD, TT, node1, node2):
-    if TD[node1] < TD[node1]:
+    if TD[node1] < TD[node2]:
         start = node1
         destiny = node2
     else:
         start = node2
         destiny = node1
 
-    print("start: ", start)
-    print("destiny: ", destiny)
-
     return findCycle(graph, TD, TT, start, destiny)
     
 if __name__ == '__main__':
     v = 500
-    graph = createRandomGraph(v)
+    graph = testGraph()
     father, TD, TT = DFS(graph)
-    for v in range(len(graph)):
-        print(TD[v], "->", TT[v])
 
-    # if hasCicle(graph, TD, TT, 6, 2):
-    #     print("ACHOU")
-    # else:
-    #     print("nao achou")
+    if hasCicle(graph, TD, TT, 0, 7):
+        print("ACHOU")
+    else:
+        print("nao achou")
