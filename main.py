7a255684be692605f87c85cf1812ee0066c7dcfd
imreadyrod <rodrigo.180santos@hotmail.com>
2024-04-14 22:11:46 -0300

    Final algorithm with method 2 to discovery articulations and the Tarjan Method

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from typing import Tuple
 
 
+
 def find_matching_positions(vector):
     
     """ 
@@ -49,6 +50,61 @@ def remove_vertex_and_return_new_graph(graph: list[list[int]],vertex:int) -> lis
     return new_graph
 
 
+
+
+def initializing_depth_first_search(graph:list[list[int]]) -> Tuple[list[int],list[int],list[int]]:
+    ''' 
+        This function has a purpose to find initializing the depth first search
+        in a graph
+
+        Parameters:
+            graph = This parameter is a list that contain each adjacency list of each
+            vertex in a graph. 
+    '''
+    list_of_discovery_time = [[] for _ in range(len(graph))]
+    list_of_fathers = [[] for _ in range(len(graph))]
+    list_of_end_time = [[] for _ in range(len(graph))]
+    time2 = [0]
+    time_of_death = [0]
+    
+
+    for vertex in range(len(graph)):
+        list_of_discovery_time[vertex]=-1
+    for vertex in range(len(graph)):
+        if list_of_discovery_time[vertex] == -1:
+            list_of_fathers[vertex] = vertex
+            depth_first_search(graph,vertex,list_of_discovery_time,list_of_fathers,
+                                list_of_end_time,time2,time_of_death)
+    
+    return list_of_discovery_time,list_of_fathers,list_of_end_time 
+        
+
+def depth_first_search(graph: list[list[int]],vertex:int,list_of_discovery_time:list[int],
+                       list_of_fathers:list[int],list_of_end_time:list[int],time2:list[int],time_of_death:list[int]) -> None:
+    ''' 
+        Performing the depth first search in graph building the 
+        vectors of pre order, post order and fathers.
+        
+
+        Parameters:
+            graph : This parameter is a list that contain each adjacency list 
+            of each vertex in a graph.
+    '''   
+        
+    list_of_discovery_time[vertex] = time2[0]
+    time2[0]+=1
+    
+    for adjacent in graph[vertex]:
+        if list_of_discovery_time[adjacent]==-1:
+                list_of_fathers[adjacent] = vertex
+                depth_first_search(graph,adjacent,list_of_discovery_time,list_of_fathers,
+                                   list_of_end_time,time2,time_of_death)
+
+
+    list_of_end_time[vertex] = time_of_death[0]
+    time_of_death[0]+=1
+
+
 def number_of_connected_components(graph:list[list[int]]) -> int:
     """ 
         Make a depth_first_search in a graph and capture the vector of fathers
@@ -61,27 +117,9 @@ def number_of_connected_components(graph:list[list[int]]) -> int:
             The number of connected components
     """    
 
-    fathers = [-1 for _ in range(len(graph))]
-    discovery = [-1 for _ in range(len(graph))]
-    time2=0
-
-    def dfs_recursive(graph, vertex,time2):
-        
-        time2+=1
-        
-        discovery[vertex] = time2
-        
-        for adjacent in graph[vertex]:
-            if discovery[adjacent]==-1:
-                    fathers[adjacent] = vertex
-                    depth_first_search(graph,adjacent)
+    _,fathers,_= initializing_depth_first_search(graph)
                     
-    for vertex in range(len(graph)):
-        if discovery[vertex] == -1:
-            dfs_recursive(graph,vertex,time2)
-            
     number_of_components = len(find_matching_positions(fathers))
-
 
     return number_of_components
 
@@ -113,60 +151,7 @@ def finding_articulations(graph: list[list[int]]) -> Tuple[list[int],int,list[in
             articulations.append(vertex)
             number_of_components_after_removal_articulation.append(new_number_of_components)
     
-    return articulations,original_number_of_components,number_of_components_after_removal_articulation
-
-def initializing_depth_first_search(graph:list[list[int]]) -> Tuple[list[int],list[int],list[int]]:
-    ''' 
-        This function has a purpose to find initializing the depth first search
-        in a graph
-
-        Parameters:
-            graph = This parameter is a list that contain each adjacency list of each
-            vertex in a graph. 
-    '''
-    list_of_discovery_time = [[] for _ in range(len(graph))]
-    list_of_fathers = [[] for _ in range(len(graph))]
-    list_of_end_time = [[] for _ in range(len(graph))]
-    time2 = 0
-    time_of_death = 0
-    
-
-    for vertex in range(len(graph)):
-        list_of_discovery_time[vertex]=-1
-    for vertex in range(len(graph)):
-        if list_of_discovery_time[vertex] == -1:
-            list_of_fathers[vertex] = vertex
-            depth_first_search(graph,vertex,list_of_discovery_time,list_of_fathers,
-                                list_of_end_time,time2,time_of_death)
-    
-    return list_of_discovery_time,list_of_fathers,list_of_end_time 
-        
-
-def depth_first_search(graph: list[list[int]],vertex:int,list_of_discovery_time:list[int],
-                       list_of_fathers:list[int],list_of_end_time:list[int],time2:int,
-                       time_of_death:int) -> None:
-    ''' 
-        Performing the depth first search in graph building the 
-        vectors of pre order, post order and fathers.
-        
-
-        Parameters:
-            graph : This parameter is a list that contain each adjacency list 
-            of each vertex in a graph.
-    '''   
-    
-    list_of_discovery_time[vertex] = time2
-    time2+=1
-    
-    for adjacent in graph[vertex]:
-        if list_of_discovery_time[adjacent]==-1:
-                list_of_fathers[adjacent] = vertex
-                depth_first_search(graph,adjacent,list_of_discovery_time,list_of_fathers,
-                                   list_of_end_time,time2,time_of_death)
-    
-    list_of_end_time[vertex] = time_of_death
-    time_of_death+=1
-    
+    return articulations,original_number_of_components,number_of_components_after_removal_articulation    
 
 def min_between_two_values(value1:int,value2:int)->int:
     """
@@ -226,7 +211,7 @@ def tarjan_method(graph:list[list[int]]) -> bool:
             graph : This parameter is a list that contain each adjacency list 
             of each vertex in a graph.
     '''    
-    lowest_preorder_number,list_of_discovery_time,list_of_fathers,list_of_end_time = finding_articulations_in_graph(graph)
+    lowest_preorder_number,list_of_discovery_time,list_of_fathers,_ = finding_articulations_in_graph(graph)
     
     for vertex in range(len(graph)):
         if ((lowest_preorder_number[vertex]==list_of_discovery_time[vertex])
@@ -290,7 +275,9 @@ if __name__ == '__main__':
     # ebc[v]   2  1  1  0  0  0  1
     
     #graph=[[2, 3],[8],[0, 3, 8],[0, 2, 8],[7, 9, 10],[6, 9],[5, 9],[4, 10],[1, 2, 3, 10],[4, 5, 6],[4, 7, 8]]
-        
+
+    #result = tarjan_method(graph)    
+    #print(result)
         #resposta    
         #  v      a  b  c  d  e  f  g   h  i  j  k
         # pre[v]  0  4  1  2  6  9  10  7  3  8  5
@@ -303,11 +290,11 @@ if __name__ == '__main__':
     
     file_path = "result_of_tarjan.txt"
     runtime=0
-
+    interactions = 100
     with open(file_path, 'w') as file:
-        number_of_vertex_to_generate = [100,1000,10000,100000]
+        number_of_vertex_to_generate = [100,1000,10000]
         for number in number_of_vertex_to_generate:
-            for estimation in range(50):
+            for estimation in range(interactions):
                 
                 graph = random_graph_generator(number)
                 
@@ -315,12 +302,12 @@ if __name__ == '__main__':
                 result = tarjan_method(graph)     
                 end_time = time.time()
                 
-                runtime += end_time - start_time
+                result2 = end_time - start_time
+                runtime += result2
                 
-                line1 = f'Graph is biconnected or not?{result}\n'
-                file.write(line1)
 
-            file.write(f'The avarage time to run the tarjan method for {number} vertex is : {runtime/number}\n\n')
+
+            file.write(f'The avarage time, to {interactions} interaction, running the tarjan method for {number} vertex is : {runtime/interactions}\n\n')
 
 
     
